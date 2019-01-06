2d7af0a49fb7aae5019b9d855cdfbed73fc88c00
Cody Yantis <cyantis@gmail.com>
2019-01-06 00:44:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(who, what){
   var theBeatlesInstruments = [];
   for(var i = 0; i < musicians.length; i ++){
-    theBeatlesInstruments.push(who[i] + " plays the " + what[i]);
+    theBeatlesInstruments.push(who[i] + " plays " + what[i]);
 }
   return theBeatlesInstruments;
 }
