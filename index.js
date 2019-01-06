242a00bd913d38bd00f9e5de63be46a942f0f5ce
Cody Yantis <cyantis@gmail.com>
2019-01-06 00:37:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(who, what){
   const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   var theBeatlesInstruments = [];
   for(i = 0; i < musicians.length; i++){
     theBeatlesInstruments.push(who[i] + " plays the " + what[i]);
