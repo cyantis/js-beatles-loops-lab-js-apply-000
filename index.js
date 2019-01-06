a3023a7579c7fc6f249c40f165524415703538ba
Cody Yantis <cyantis@gmail.com>
2019-01-06 00:28:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(who, what){
+  var theBeatlesInstruments = [];
+  for(i = 0; i < musicians.length; i++){
+    theBeatlesInstruments.push(who[i] + " plays the " + what[i]);
+}
+  return theBeatlesInstruments;
+}
